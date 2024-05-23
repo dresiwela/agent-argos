c14c54c5f50d947aec26030dafdfe9ded4ae8a87
dresiwela <20170116@student.curtin.edu.au>
2024-05-23 11:20:47 +0800

    apply offset

--- agent.py
+++ agent.py
@@ -84,11 +84,15 @@ for payload in data_stream:
     now = time.time()
     frame_data = {}
 
+    lat_offset = 6.424739041221983e-05
+    long_offset = 7.77840633361393e-05
     for tracked_object in tracked_objects:
         pixel_coordinate = tracked_object.estimate[0]
         class_id = tracked_object.label
         obj_id = tracked_object.id
         lat, long = pixel_to_gps(pixel_coordinate, K, dist, Hsat2cctv_inv, T_gps2sat_inv)
+        lat = lat + lat_offset
+        long = long + long_offset
         print(pixel_coordinate,lat,long)
 
         if obj_id not in prev_coordinates:
